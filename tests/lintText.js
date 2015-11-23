aac74fb64bf7391d176447e0cce8b678581b87b9
Gajus Kuizinas <gk@anuary.com>
2015-11-23 14:44:07 +0000

    Enable tests.

---
+++ tests/lintText.js
@@ -0,0 +1,19 @@
+import {
+    expect
+} from 'chai';
+
+import {
+    lintText
+} from './../src';
+
+describe('lintText', () => {
+    context('unrecognized linter', () => {
+        it('throws an error', () => {
+            expect(() => {
+                lintText('', {
+                    linter: 'java'
+                });
+            }).to.throw(Error, 'Unknown linter "java".');
+        });
+    });
+});
