79be01635e748ffa39a33f576b2f1979f549cce5
Gajus Kuizinas <gk@anuary.com>
2015-11-27 17:50:00 +0000

    Handle failing stdin process.

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,34 +1,50 @@
 #! /usr/bin/env node
 
 import getStdin from 'get-stdin';
+import chalk from 'chalk';
 
 import {
     lintFiles,
-    lintText,
-    getFormatter
+    lintText
 } from './../';
 
 import yargs, {
     argv
 } from 'yargs';
 
-import glob from 'globby';
-import path from 'path';
-import _ from 'lodash';
+import getTargetPaths from './../utilities/bin/getTargetPaths';
+import outputReport from './../utilities/bin/outputReport';
 
-let getTargetPaths,
-    outputReport,
-    resolveAbsolutePath,
-    targetPaths;
+let targetPaths;
 
+process.on('unhandledRejection', (reason, promise) => {
+    /* eslint-disable no-console */
+    console.log(chalk.red('Unhandled'), reason, promise);
+    /* eslint-enable */
+
+    throw reason;
+});
+
+/* eslint-disable no-unused-expressions */
 yargs
+/* eslint-enable */
+    .fail((message) => {
+        /* eslint-disable no-console */
+        console.log(chalk.red('Error'), message);
+        /* eslint-enable */
+
+        /* eslint-disable no-process-exit */
+        process.exit(1);
+        /* eslint-enable */
+    })
+    .strict()
     .options({
         stdin: {
             describe: 'Used to indicate that subject body will be read from stdin.',
             type: 'boolean',
             default: false
         },
-        outputFormat: {
+        'output-format': {
             choices: [
                 'json',
                 'checkstyle',
@@ -36,115 +52,27 @@ yargs
             ],
             default: 'table'
         },
-        filePath: {
-            describe: 'Name of the file being linted with stdin (if any). Used in reporting',
+        'file-path': {
+            describe: 'Name of the file being linted with stdin (if any). Used in reporting.',
             type: 'string',
             default: '<text>'
+        },
+        linter: {
+            demand: true,
+            describe: 'The type of input.',
+            choices: [
+                'js',
+                'scss'
+            ]
         }
     })
     .argv;
 
-/**
- * @param {string} relativePath Path relative to the process.cwd()
- * @returns {string} Absolute path.
- */
-resolveAbsolutePath = (relativePath) => {
-    return path.resolve(process.cwd(), relativePath);
-};
-
-/**
- * @returns {string[]}
- */
-getTargetPaths = () => {
-    let appendPaths,
-        paths;
-
-    appendPaths = [];
-
-    if (argv._.length) {
-        paths = argv._;
-    } else {
-        paths = ['./'];
-    }
-
-    paths = _.filter(paths, (pathName) => {
-        let exclude;
-
-        exclude = _.endsWith(pathName, '/') === true;
-
-        if (exclude) {
-            appendPaths.push(pathName + '**/*.js');
-            appendPaths.push(pathName + '**/*.css');
-            appendPaths.push(pathName + '**/*.scss');
-        }
-
-        return !exclude;
-    });
-
-    paths = paths.concat(appendPaths);
-    paths = _.unique(paths);
-
-    paths = glob.sync(paths);
-    // @todo Test whether glob.sync can return non-unique file paths.
-    // paths = _.unique(paths);
-
-    // console.log('paths', paths);
-
-    paths = _.map(paths, resolveAbsolutePath);
-
-    // console.log('paths', paths);
-
-    return paths;
-};
-
-/**
- * @param {Object} report
- * @returns {undefined}
- */
-outputReport = (report) => {
-    let formatter,
-        output;
-
-    if (argv.outputFormat === 'json') {
-        output = JSON.stringify({
-            messages: report.messages,
-            errorCount: report.errorCount,
-            warningCount: report.warningCount
-        }, '', 4);
-    } else {
-        formatter = getFormatter(argv.outputFormat);
-
-        if (argv.stdin) {
-            report.filePath = argv.filePath;
-            output = formatter({
-                results: [
-                    report
-                ],
-                errorCount: report.errorCount,
-                warningCount: report.warningCount
-            });
-        } else {
-            output = formatter(report);
-        }
-    }
-
-    /* eslint-disable no-console */
-    console.log(output);
-    /* eslint-enable no-console */
-};
-
 if (argv.stdin) {
+    /* eslint-disable no-unused-expressions */
     yargs
-        .options({
-            linter: {
-                demand: true,
-                describe: 'The type of input.',
-                choices: [
-                    'js',
-                    'scss'
-                ]
-            }
-        })
+    /* eslint-enable */
+        .demand(0, 0)
         .argv;
 
     getStdin()
@@ -152,21 +80,30 @@ if (argv.stdin) {
             let report;
 
             report = lintText(stdin, {
-                linter: argv.linter
+                linter: argv.linter,
+                filePath: argv.filePath
             });
 
-            // console.log('BEST', stdin, argv.linter, lintText);
-
-            outputReport(report);
+            outputReport({
+                results: [
+                    report
+                ],
+                errorCount: report.errorCount,
+                warningCount: report.warningCount
+            }, argv.outputFormat);
         });
 } else {
     let report;
 
-    targetPaths = getTargetPaths();
+    /* eslint-disable no-unused-expressions */
+    yargs
+    /* eslint-enable */
+        .demand(1)
+        .argv;
 
-    // console.log('targetPaths', targetPaths);
+    targetPaths = getTargetPaths(argv._);
 
     report = lintFiles(targetPaths);
 
-    outputReport(report);
+    outputReport(report, argv.outputFormat);
 }
