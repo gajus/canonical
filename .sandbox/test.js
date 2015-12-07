393c5f30918fc2410e8f368c2fd42c614721718a
Gajus Kuizinas <gk@anuary.com>
2015-12-07 13:50:57 +0000

    Added root: true to eslint to ensure that no rules are inherited.

--- .sandbox/test.js
+++ .sandbox/test.js
@@ -1,1 +1,10 @@
+import chalk from 'chalk';
+import test2 from './test.scss';
 import test from './test';
+
+export let a;
+export let a;
+
+export default () => {
+    return <div></div>;
+};
--- src/linters/js/index.js
+++ src/linters/js/index.js
@@ -2,16 +2,20 @@ import {
     CLIEngine
 } from 'eslint';
 
-import path from 'path';
-
 let cli,
     lintText;
 
 cli = new CLIEngine({
-    useElintrc: false,
-    // baseConfig: false,
-    parser: 'babel-eslint',
-    baseConfig: path.resolve(__dirname, './eslintrc.json')
+    configFile: require.resolve('./eslintrc.json'),
+    baseConfig: false,
+    rulePaths: [],
+    useEslintrc: false,
+    envs: [],
+    globals: [],
+    extensions: [],
+    parser: require.resolve('babel-eslint'),
+    fix: false,
+    allowInlineConfig: true
 });
 
 /**
@@ -19,8 +23,6 @@ cli = new CLIEngine({
  * @returns {string}
  */
 lintText = (text) => {
-    // console.log('cli.executeOnText(text).results[0]', cli.executeOnText(text).results[0]);
-
     return cli.executeOnText(text).results[0];
 };
 
