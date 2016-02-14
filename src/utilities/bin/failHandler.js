7f4d96bcf3f9423908fab549b8a8959017468cd5
Gajus Kuizinas <gajus@gajus.com>
2016-02-14 14:15:07 +0000

    Error object is not always present.

--- src/utilities/bin/failHandler.js
+++ src/utilities/bin/failHandler.js
@@ -2,6 +2,10 @@ import chalk from 'chalk';
 
 export default (message, err) => {
     /* eslint-disable no-console */
-    console.log(chalk.red('Error'), message, err.stack);
+    if (err) {
+        console.log(chalk.red('Error'), message, err.stack);
+    } else {
+        console.log(chalk.red('Error'), message);
+    }
     /* eslint-enable */
 };
